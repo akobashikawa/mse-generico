199411de14fa044df93ad9cd9d14c273c755e108
Antonio Kobashikawa <akobashikawa@gmail.com>
2024-11-13 10:36:25 -0500

    Configura cors en monolito

--- backend/monolito/src/main/java/com/example/mse/monolito/AppConfig.java
+++ backend/monolito/src/main/java/com/example/mse/monolito/AppConfig.java
@@ -3,6 +3,8 @@ package com.example.mse.monolito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,19 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite CORS en todas las rutas
+                        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000") // Origen permitido
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Cabeceras permitidas
+                        .allowCredentials(true); // Permite cookies y autenticación
+            }
+        };
     }
 }
