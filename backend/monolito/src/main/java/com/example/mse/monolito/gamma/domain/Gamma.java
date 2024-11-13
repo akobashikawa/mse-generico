852e9051ed0bcda9968c29fbdf641eb332f69ed6
Antonio Kobashikawa <akobashikawa@gmail.com>
2024-11-13 14:51:19 -0500

    Relaciona Alfa con Gamma

--- backend/monolito/src/main/java/com/example/mse/monolito/gamma/domain/Gamma.java
+++ backend/monolito/src/main/java/com/example/mse/monolito/gamma/domain/Gamma.java
@@ -1,10 +1,15 @@
 package com.example.mse.monolito.gamma.domain;
 
 
+import com.example.mse.monolito.alfa.domain.Alfa;
+import com.example.mse.monolito.beta.domain.Beta;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -19,5 +24,13 @@ public class Gamma {
 	private String texto;
 	private Integer entero;
 	private Double decimal;
+	
+	@ManyToOne
+	@JoinColumn(name = "alfa_id", nullable = false)
+	private Alfa alfa;
+	
+//	@ManyToOne
+//	@JoinColumn(name = "beta_id", nullable = false)
+//	private Beta beta;
 
 }
